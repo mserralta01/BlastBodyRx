mserralta01/trimpath

feat: add connected peptide commerce management

convex/admin.ts
@@ -2,9 +2,9 @@ import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 import { requireTrimPathAdmin } from "./lib/auth";
 
-export const customers = query({ args: {}, handler: async (ctx) => { await requireTrimPathAdmin(ctx); return ctx.db.query("customers").collect(); } });
-export const discounts = query({ args: {}, handler: async (ctx) => { await requireTrimPathAdmin(ctx); return ctx.db.query("discounts").collect(); } });
-export const batches = query({ args: {}, handler: async (ctx) => { await requireTrimPathAdmin(ctx); return ctx.db.query("batches").collect(); } });
+export const customers = query({ args: {}, handler: async (ctx) => { await requireTrimPathAdmin(ctx); return ctx.db.query("customers").take(500); } });
+export const discounts = query({ args: {}, handler: async (ctx) => { await requireTrimPathAdmin(ctx); return ctx.db.query("discounts").take(250); } });
+export const batches = query({ args: {}, handler: async (ctx) => { await requireTrimPathAdmin(ctx); return ctx.db.query("batches").take(500); } });
 export const settings = query({ args: {}, handler: async (ctx) => { await requireTrimPathAdmin(ctx); return ctx.db.query("storeSettings").withIndex("by_singleton", (q) => q.eq("singleton", "main")).unique(); } });
 
 export const createDiscount = mutation({

convex/catalog.ts
@@ -2,28 +2,14 @@ import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 import { requireTrimPathAdmin } from "./lib/auth";
 
-export const list = query({
-  args: {},
-  handler: async (ctx) => ctx.db.query("products").collect(),
-});
-
-export const updateInventory = mutation({
-  args: { productId: v.id("products"), sku: v.string(), inventory: v.number() },
-  handler: async (ctx, args) => {
-    await requireTrimPathAdmin(ctx);
-    const product = await ctx.db.get(args.productId);
-    if (!product) throw new Error("Product not found");
-    await ctx.db.patch(args.productId, {
-      variants: product.variants.map((item) => item.sku === args.sku ? { ...item, inventory: Math.max(0, Math.floor(args.inventory)) } : item),
-      updatedAt: Date.now(),
-    });
-  },
-});
-
-export const toggleActive = mutation({
-  args: { productId: v.id("products"), active: v.boolean() },
-  handler: async (ctx, args) => {
-    await requireTrimPathAdmin(ctx);
-    return ctx.db.patch(args.productId, { active: args.active, updatedAt: Date.now() });
-  },
-});
+const category = v.union(v.literal("GLP-1"), v.literal("Peptides"));
+const variant = v.object({ sku: v.string(), strength: v.string(), price: v.number(), inventory: v.number(), lowStockAt: v.number(), active: v.boolean() });
+export const list = query({ args: {}, handler: async (ctx) => { await requireTrimPathAdmin(ctx); return ctx.db.query("products").take(250); } });
+export const listActive = query({ args: {}, handler: async (ctx) => { const records = await ctx.db.query("products").withIndex("by_active", (q) => q.eq("active", true)).take(250); return records.sort((a, b) => (a.sortOrder ?? 999) - (b.sortOrder ?? 999) || a.name.localeCompare(b.name)); } });
+export const getActiveBySlug = query({ args: { slug: v.string() }, handler: async (ctx, args) => { const product = await ctx.db.query("products").withIndex("by_slug", (q) => q.eq("slug", args.slug)).unique(); return product?.active ? product : null; } });
+export const save = mutation({ args: { productId: v.optional(v.id("products")), slug: v.string(), name: v.string(), category, description: v.string(), image: v.string(), badge: v.string(), featured: v.boolean(), tags: v.array(v.string()), seoTitle: v.string(), seoDescription: v.string(), sortOrder: v.number(), active: v.boolean(), variants: v.array(variant) }, handler: async (ctx, args) => {
+  await requireTrimPathAdmin(ctx); const { productId, ...record } = args; const slug = record.slug.trim().toLowerCase().replace(/[^a-z0-9]+/g, "-").replace(/(^-|-$)/g, ""); const clean = { ...record, slug, variants: record.variants.map((item) => ({ ...item, price: Math.max(0, item.price), inventory: Math.max(0, Math.floor(item.inventory)), lowStockAt: Math.max(0, Math.floor(item.lowStockAt)) })), updatedAt: Date.now() };
+  if (!clean.name.trim() || !clean.slug || !clean.variants.length) throw new Error("Name, slug, and at least one variant are required"); const duplicate = await ctx.db.query("products").withIndex("by_slug", (q) => q.eq("slug", clean.slug)).unique(); if (duplicate && duplicate._id !== productId) throw new Error("That product slug is already in use"); if (productId) { await ctx.db.patch(productId, clean); return productId; } return ctx.db.insert("products", clean);
+} });
+export const updateInventory = mutation({ args: { productId: v.id("products"), sku: v.string(), inventory: v.number() }, handler: async (ctx, args) => { await requireTrimPathAdmin(ctx); const product = await ctx.db.get(args.productId); if (!product) throw new Error("Product not found"); await ctx.db.patch(args.productId, { variants: product.variants.map((item) => item.sku === args.sku ? { ...item, inventory: Math.max(0, Math.floor(args.inventory)) } : item), updatedAt: Date.now() }); } });
+export const toggleActive = mutation({ args: { productId: v.id("products"), active: v.boolean() }, handler: async (ctx, args) => { await requireTrimPathAdmin(ctx); return ctx.db.patch(args.productId, { active: args.active, updatedAt: Date.now() }); } });

src/components/storefront-home.tsx
@@ -1,0 +1,19 @@
+"use client";
+import Link from "next/link";
+import { useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { ProductCard } from "@/components/product-card";
+import { StoreLayout } from "@/components/store-shell";
+import { TrimPathVial } from "@/components/trimpath-vial";
+import { products as fallback, type Product } from "@/lib/catalog";
+
+export function StorefrontHome() {
+  const liveProducts = useQuery(api.catalog.listActive); const commerce = useQuery(api.commerce.publicStorefront); const products = (liveProducts?.length ? liveProducts : fallback) as Array<Product & { featured?: boolean }>; const sections = commerce?.sections ?? [];
+  const hero = sections.find((section) => section.type === "hero" && section.enabled); const featured = sections.find((section) => section.type === "featured" && section.enabled); const trust = sections.find((section) => section.type === "trust" && section.enabled); const certificates = sections.find((section) => section.type === "certificates" && section.enabled);
+  return <StoreLayout><main>
+    {hero && <section className="hero"><div className="container hero__grid"><div className="hero__copy"><span className="pill">Documentation-first research supply</span><h1>{hero.title}</h1><p>{hero.subtitle}</p><div className="hero__actions"><Link className="button button--primary" href="/shop">Shop compounds</Link><Link className="button button--white" href="/lab-results">View batch COAs</Link></div><div className="hero__proof"><span>✓ {products.reduce((sum, item) => sum + item.variants.length, 0)} current research SKUs</span><span>✓ Batch-linked documentation</span><span>✓ Configurable U.S. shipping</span></div></div><div className="hero__visual"><div className="hero-ring" /><TrimPathVial className="hero-vial hero-vial--rear" name={products[0]?.name ?? "Retatrutide"} strength={products[0]?.variants[0]?.strength ?? "20mg"} /><TrimPathVial className="hero-vial hero-vial--front" name={products[1]?.name ?? "Tesamorelin"} strength={products[1]?.variants[0]?.strength ?? "10mg"} /><div className="research-seal"><small>TRIM PATH RX</small><strong>RESEARCH</strong><small>USE ONLY</small></div></div></div></section>}
+    {featured && <section className="section catalogue-section"><div className="container"><div className="section-heading centered"><span className="eyebrow eyebrow--blue">Current catalogue</span><h2>{featured.title}</h2><p>{featured.subtitle}</p></div><div className="product-grid">{products.filter((item) => item.featured !== false).slice(0, 8).map((product) => <ProductCard product={product} key={product.slug} />)}</div><div className="centered section-cta"><Link href="/shop" className="button button--outline">View all {products.length} compounds</Link></div></div></section>}
+    {trust && <section className="section workflow-section"><div className="container"><div className="section-heading"><span className="eyebrow eyebrow--blue">Peptide commerce, connected</span><h2>{trust.title}</h2><p>{trust.subtitle}</p></div><div className="feature-grid">{[["01","Live product catalog","Products, variants, prices, stock, and availability flow directly from the back office."],["02","Canonical checkout","Every price is recalculated from Convex before Stripe checkout is created."],["03","Batch documentation","Certificates stay searchable by compound and lot number."],["04","Fulfillment controls","Shipping choices, payment state, customers, and order status share one operational record."]].map(([number,title,copy]) => <article key={number}><span>{number}</span><h3>{title}</h3><p>{copy}</p></article>)}</div></div></section>}
+    {certificates && <section className="section certificates-section"><div className="container certificates-grid"><div><span className="eyebrow">Batch documentation</span><h2>{certificates.title}</h2><p>{certificates.subtitle}</p><Link className="button button--white" href="/lab-results">Browse certificates</Link></div><div className="certificate-stats"><div><strong>{products.length}</strong><span>Compounds</span></div><div><strong>{products.reduce((sum,item)=>sum+item.variants.length,0)}</strong><span>Current SKUs</span></div><div><strong>1</strong><span>Connected catalog</span></div></div></div></section>}
+  </main></StoreLayout>;
+}

mserralta01/BlastBodyRx

convex/admin.ts
@@ -2,9 +2,9 @@ import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 import { requireVirtualFitPathAdmin } from "./lib/auth";
 
-export const customers = query({ args: {}, handler: async (ctx) => { await requireVirtualFitPathAdmin(ctx); return ctx.db.query("customers").collect(); } });
-export const discounts = query({ args: {}, handler: async (ctx) => { await requireVirtualFitPathAdmin(ctx); return ctx.db.query("discounts").collect(); } });
-export const batches = query({ args: {}, handler: async (ctx) => { await requireVirtualFitPathAdmin(ctx); return ctx.db.query("batches").collect(); } });
+export const customers = query({ args: {}, handler: async (ctx) => { await requireVirtualFitPathAdmin(ctx); return ctx.db.query("customers").take(500); } });
+export const discounts = query({ args: {}, handler: async (ctx) => { await requireVirtualFitPathAdmin(ctx); return ctx.db.query("discounts").take(250); } });
+export const batches = query({ args: {}, handler: async (ctx) => { await requireVirtualFitPathAdmin(ctx); return ctx.db.query("batches").take(500); } });
 export const settings = query({ args: {}, handler: async (ctx) => { await requireVirtualFitPathAdmin(ctx); return ctx.db.query("storeSettings").withIndex("by_singleton", (q) => q.eq("singleton", "main")).unique(); } });
 
 export const createDiscount = mutation({

convex/catalog.ts
@@ -2,28 +2,15 @@ import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 import { requireVirtualFitPathAdmin } from "./lib/auth";
 
-export const list = query({
-  args: {},
-  handler: async (ctx) => ctx.db.query("products").collect(),
-});
-
-export const updateInventory = mutation({
-  args: { productId: v.id("products"), sku: v.string(), inventory: v.number() },
-  handler: async (ctx, args) => {
-    await requireVirtualFitPathAdmin(ctx);
-    const product = await ctx.db.get(args.productId);
-    if (!product) throw new Error("Product not found");
-    await ctx.db.patch(args.productId, {
-      variants: product.variants.map((item) => item.sku === args.sku ? { ...item, inventory: Math.max(0, Math.floor(args.inventory)) } : item),
-      updatedAt: Date.now(),
-    });
-  },
-});
-
-export const toggleActive = mutation({
-  args: { productId: v.id("products"), active: v.boolean() },
-  handler: async (ctx, args) => {
-    await requireVirtualFitPathAdmin(ctx);
-    return ctx.db.patch(args.productId, { active: args.active, updatedAt: Date.now() });
-  },
-});
+const category = v.union(v.literal("GLP-1"), v.literal("Peptides"));
+const variant = v.object({ sku: v.string(), strength: v.string(), price: v.number(), inventory: v.number(), lowStockAt: v.number(), active: v.boolean() });
+export const list = query({ args: {}, handler: async (ctx) => { await requireVirtualFitPathAdmin(ctx); return ctx.db.query("products").take(250); } });
+export const listActive = query({ args: {}, handler: async (ctx) => { const records = await ctx.db.query("products").withIndex("by_active", (q) => q.eq("active", true)).take(250); return records.sort((a, b) => (a.sortOrder ?? 999) - (b.sortOrder ?? 999) || a.name.localeCompare(b.name)); } });
+export const getActiveBySlug = query({ args: { slug: v.string() }, handler: async (ctx, args) => { const product = await ctx.db.query("products").withIndex("by_slug", (q) => q.eq("slug", args.slug)).unique(); return product?.active ? product : null; } });
+export const save = mutation({ args: { productId: v.optional(v.id("products")), slug: v.string(), name: v.string(), category, description: v.string(), image: v.string(), badge: v.string(), featured: v.boolean(), tags: v.array(v.string()), seoTitle: v.string(), seoDescription: v.string(), sortOrder: v.number(), active: v.boolean(), variants: v.array(variant) }, handler: async (ctx, args) => {
+  await requireVirtualFitPathAdmin(ctx); const { productId, ...record } = args; const slug = record.slug.trim().toLowerCase().replace(/[^a-z0-9]+/g, "-").replace(/(^-|-$)/g, "");
+  const clean = { ...record, slug, variants: record.variants.map((item) => ({ ...item, price: Math.max(0, item.price), inventory: Math.max(0, Math.floor(item.inventory)), lowStockAt: Math.max(0, Math.floor(item.lowStockAt)) })), updatedAt: Date.now() };
+  if (!clean.name.trim() || !clean.slug || !clean.variants.length) throw new Error("Name, slug, and at least one variant are required"); const duplicate = await ctx.db.query("products").withIndex("by_slug", (q) => q.eq("slug", clean.slug)).unique(); if (duplicate && duplicate._id !== productId) throw new Error("That product slug is already in use"); if (productId) { await ctx.db.patch(productId, clean); return productId; } return ctx.db.insert("products", clean);
+} });
+export const updateInventory = mutation({ args: { productId: v.id("products"), sku: v.string(), inventory: v.number() }, handler: async (ctx, args) => { await requireVirtualFitPathAdmin(ctx); const product = await ctx.db.get(args.productId); if (!product) throw new Error("Product not found"); await ctx.db.patch(args.productId, { variants: product.variants.map((item) => item.sku === args.sku ? { ...item, inventory: Math.max(0, Math.floor(args.inventory)) } : item), updatedAt: Date.now() }); } });
+export const toggleActive = mutation({ args: { productId: v.id("products"), active: v.boolean() }, handler: async (ctx, args) => { await requireVirtualFitPathAdmin(ctx); return ctx.db.patch(args.productId, { active: args.active, updatedAt: Date.now() }); } });

src/components/storefront-home.tsx
@@ -1,0 +1,19 @@
+"use client";
+import Link from "next/link";
+import { useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { ProductCard } from "@/components/product-card";
+import { StoreLayout } from "@/components/store-shell";
+import { VirtualFitPathVial } from "@/components/virtualfitpath-vial";
+import { products as fallback, type Product } from "@/lib/catalog";
+
+export function StorefrontHome() {
+  const liveProducts = useQuery(api.catalog.listActive); const commerce = useQuery(api.commerce.publicStorefront); const products = (liveProducts?.length ? liveProducts : fallback) as Array<Product & { featured?: boolean }>; const sections = commerce?.sections ?? [];
+  const hero = sections.find((section) => section.type === "hero" && section.enabled); const featured = sections.find((section) => section.type === "featured" && section.enabled); const trust = sections.find((section) => section.type === "trust" && section.enabled); const certificates = sections.find((section) => section.type === "certificates" && section.enabled);
+  return <StoreLayout><main>
+    {hero && <section className="hero"><div className="container hero__grid"><div className="hero__copy"><span className="pill">Documentation-first research supply</span><h1>{hero.title}</h1><p>{hero.subtitle}</p><div className="hero__actions"><Link className="button button--primary" href="/shop">Shop compounds</Link><Link className="button button--white" href="/lab-results">View batch COAs</Link></div><div className="hero__proof"><span>✓ {products.reduce((sum, item) => sum + item.variants.length, 0)} current research SKUs</span><span>✓ Batch-linked documentation</span><span>✓ Configurable U.S. shipping</span></div></div><div className="hero__visual"><div className="hero-ring" /><VirtualFitPathVial className="hero-vial hero-vial--rear" name={products[0]?.name ?? "Retatrutide"} strength={products[0]?.variants[0]?.strength ?? "20mg"} /><VirtualFitPathVial className="hero-vial hero-vial--front" name={products[1]?.name ?? "Tesamorelin"} strength={products[1]?.variants[0]?.strength ?? "10mg"} /><div className="research-seal"><small>VIRTUAL FIT PATH</small><strong>RESEARCH</strong><small>USE ONLY</small></div></div></div></section>}
+    {featured && <section className="section catalogue-section"><div className="container"><div className="section-heading centered"><span className="eyebrow eyebrow--blue">Current catalogue</span><h2>{featured.title}</h2><p>{featured.subtitle}</p></div><div className="product-grid">{products.filter((item) => item.featured !== false).slice(0, 8).map((product) => <ProductCard product={product} key={product.slug} />)}</div><div className="centered section-cta"><Link href="/shop" className="button button--outline">View all {products.length} compounds</Link></div></div></section>}
+    {trust && <section className="section workflow-section"><div className="container"><div className="section-heading"><span className="eyebrow eyebrow--blue">Peptide commerce, connected</span><h2>{trust.title}</h2><p>{trust.subtitle}</p></div><div className="feature-grid">{[["01","Live product catalog","Products, variants, prices, stock, and availability flow directly from the back office."],["02","Canonical checkout","Every price is recalculated from Convex before Stripe checkout is created."],["03","Batch documentation","Certificates stay searchable by compound and lot number."],["04","Fulfillment controls","Shipping choices, payment state, customers, and order status share one operational record."]].map(([number,title,copy]) => <article key={number}><span>{number}</span><h3>{title}</h3><p>{copy}</p></article>)}</div></div></section>}
+    {certificates && <section className="section certificates-section"><div className="container certificates-grid"><div><span className="eyebrow">Batch documentation</span><h2>{certificates.title}</h2><p>{certificates.subtitle}</p><Link className="button button--white" href="/lab-results">Browse certificates</Link></div><div className="certificate-stats"><div><strong>{products.length}</strong><span>Compounds</span></div><div><strong>{products.reduce((sum,item)=>sum+item.variants.length,0)}</strong><span>Current SKUs</span></div><div><strong>1</strong><span>Connected catalog</span></div></div></div></section>}
+  </main></StoreLayout>;
+}